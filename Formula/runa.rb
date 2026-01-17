eb96db9478a29e36fe741caa33cda34b4748dad9
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-01-17 04:18:46 +0100

    Updated runa-bin to runa instead
    Now installs only the runa binaries

--- Formula/runa.rb
+++ Formula/runa.rb
@@ -1,20 +1,41 @@
 class Runa < Formula
-  desc "Fast and lightweight console file browser written in Rust"
+  desc "Fast and lightweight terminal file browser written in Rust"
   homepage "https://github.com/alexm-dev/runa"
-  url "https://github.com/alexm-dev/runa/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "4afe8a0b86e5484fcbf02c03788f50a0b7b25ddf4376b51fc7eab05907b3d20b"
   license any_of: ["MIT", "Apache-2.0"]
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-macos-aarch64.tar.gz"
+      sha256 "41dc2e95e171fc789d6704da4a9741325ef738ff0313383f8612aa2ccbc82245"
+    else
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-macos-x86_64.tar.gz"
+      sha256 "4c77ec0fc3c68f37e17a23a24d2512cb53ffdcd45fba52a3bcd4d712a2a17a91"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-linux-aarch64.tar.gz"
+      sha256 "f3c47acbba1b0dd58459014967fd2e589cbd07d21d2def400ea720164023db67"
+    else
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-linux-x86_64.tar.gz"
+      sha256 "08e0ad66c4806f80ad2e4c261ec27874844834e54a43c3b514c19a9ffd59cf2a"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
-    doc.install "README.md", "docs/configuration.md"
+    bin.install "rn"
+
+    doc.install "LICENSE-MIT" if File.exist? "LICENSE-MIT"
+    doc.install "LICENSE-APACHE" if File.exist? "LICENSE-APACHE"
+    doc.install "README.md" if File.exist? "README.md"
+
+    if File.directory? "docs"
+      doc.install "docs/configuration.md" if File.exist? "docs/configuration.md"
+    end
   end
 
   test do
-    assert_predicate bin/"rn", :exist?
-    assert_predicate bin/"rn", :executable?
     assert_match "Usage", shell_output("#{bin}/rn --help")
   end
 end
