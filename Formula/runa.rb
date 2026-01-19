6e8bcfd082a9ab67cf64c6e2c7c7baa80885b76e
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-01-19 05:56:02 +0100

    chore: Updated runa formula to runa version v0.5.5

--- Formula/runa.rb
+++ Formula/runa.rb
@@ -5,21 +5,21 @@ class Runa < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-macos-aarch64.tar.gz"
-      sha256 "41dc2e95e171fc789d6704da4a9741325ef738ff0313383f8612aa2ccbc82245"
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-macos-aarch64.tar.gz"
+      sha256 "b04eb48e597c4a3e7574be43ed3f3d30e1bba5bfd7506f6c574d86af3157dfd6"
     else
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-macos-x86_64.tar.gz"
-      sha256 "4c77ec0fc3c68f37e17a23a24d2512cb53ffdcd45fba52a3bcd4d712a2a17a91"
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-macos-x86_64.tar.gz"
+      sha256 "ca8b751a314092f5258c73e50526fd1cec720b9b2c25adcf59c94a022d8e632d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-linux-aarch64.tar.gz"
-      sha256 "f3c47acbba1b0dd58459014967fd2e589cbd07d21d2def400ea720164023db67"
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-linux-aarch64.tar.gz"
+      sha256 "aa6354bd44a1d73d612e34e782d5a34a090127b78604e3dd70beb76f32ab9399"
     else
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.3/runa-linux-x86_64.tar.gz"
-      sha256 "08e0ad66c4806f80ad2e4c261ec27874844834e54a43c3b514c19a9ffd59cf2a"
+      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-linux-x86_64.tar.gz"
+      sha256 "943b8fb6667432b5dfab7369ad7ab4d8fa860f016f772337e6c5eb5911477a36"
     end
   end
 
