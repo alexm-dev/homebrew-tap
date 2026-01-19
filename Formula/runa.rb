af40ba0fcc90bcc8f86dd3c4b5a647d9cd3fa4fc
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-01-19 06:05:05 +0100

    chore: Updated formula to now show the current version as well

--- Formula/runa.rb
+++ Formula/runa.rb
@@ -1,24 +1,25 @@
 class Runa < Formula
   desc "Fast and lightweight terminal file browser written in Rust"
   homepage "https://github.com/alexm-dev/runa"
+  version "0.5.5"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-macos-aarch64.tar.gz"
+      url "https://github.com/alexm-dev/runa/releases/download/v#{version}/runa-macos-aarch64.tar.gz"
       sha256 "b04eb48e597c4a3e7574be43ed3f3d30e1bba5bfd7506f6c574d86af3157dfd6"
     else
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-macos-x86_64.tar.gz"
+      url "https://github.com/alexm-dev/runa/releases/download/v#{version}/runa-macos-x86_64.tar.gz"
       sha256 "ca8b751a314092f5258c73e50526fd1cec720b9b2c25adcf59c94a022d8e632d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-linux-aarch64.tar.gz"
+      url "https://github.com/alexm-dev/runa/releases/download/v#{version}/runa-linux-aarch64.tar.gz"
       sha256 "aa6354bd44a1d73d612e34e782d5a34a090127b78604e3dd70beb76f32ab9399"
     else
-      url "https://github.com/alexm-dev/runa/releases/download/v0.5.5/runa-linux-x86_64.tar.gz"
+      url "https://github.com/alexm-dev/runa/releases/download/v#{version}/runa-linux-x86_64.tar.gz"
       sha256 "943b8fb6667432b5dfab7369ad7ab4d8fa860f016f772337e6c5eb5911477a36"
     end
   end
@@ -37,5 +38,6 @@ class Runa < Formula
 
   test do
     assert_match "Usage", shell_output("#{bin}/rn --help")
+    assert_match version.to_s, shell_output("#{bin}/rn --version")
   end
 end
